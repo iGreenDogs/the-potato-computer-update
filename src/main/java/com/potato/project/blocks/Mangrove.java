cdb9542a09649acd561f2d920c13dab101abedd8
Roee Mamet <roeemamet@gmail.com>
2022-04-17 08:34:47 -0400

    Fixed Log and Door textures

--- src/main/java/com/potato/project/blocks/Mangrove.java
+++ src/main/java/com/potato/project/blocks/Mangrove.java
@@ -1,14 +1,15 @@
 package com.potato.project.blocks;
 
 import com.potato.project.blocks.weird.ModDoorBlock;
+import com.potato.project.blocks.weird.ModPillarBlock;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
 
 public class Mangrove {
-    public static final Block MANGROVE_LOG = new Block(FabricBlockSettings.of(Material.WOOD).strength(1.0f));
+    public static final ModPillarBlock MANGROVE_LOG = new ModPillarBlock(FabricBlockSettings.of(Material.WOOD).strength(1.0f));
     public static final Block MANGROVE_PLANKS = new Block(FabricBlockSettings.of(Material.WOOD).strength(1.0f));
     public static final Block MANGROVE_ROOTS = new Block(FabricBlockSettings.of(Material.WOOD).strength(1.0f).nonOpaque());
-    public static final Block MANGROVE_DOOR = new ModDoorBlock(FabricBlockSettings.of(Material.WOOD).strength(1.0f).nonOpaque());
+    public static final ModDoorBlock MANGROVE_DOOR = new ModDoorBlock(FabricBlockSettings.of(Material.WOOD).strength(1.0f).nonOpaque());
     public static final Block MANGROVE_LEAVES = new Block(FabricBlockSettings.of(Material.WOOD).strength(1.0f).ticksRandomly().nonOpaque());
 }
---
+++ src/main/java/com/potato/project/blocks/weird/ModPillarBlock.java
@@ -0,0 +1,9 @@
+package com.potato.project.blocks.weird;
+
+import net.minecraft.block.PillarBlock;
+
+public class ModPillarBlock extends PillarBlock {
+    public ModPillarBlock(Settings settings) {
+        super(settings);
+    }
+}
